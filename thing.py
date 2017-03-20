dd3b4ee704f5ed0e30fada4a2ec231058354dd29
Jack Williams <hunter2@national,shitposting,agency>
2017-03-20 19:52:33 +1100

    no

--- thing.py
+++ thing.py
@@ -13,13 +13,10 @@ def on_ready():
 @client.event
 @asyncio.coroutine
 def on_message(message):
-    print('saw message')
+    if message.content in shit_words:
+        yield from client.send_message(message.channel, 'you have entered the shit list.', tts=True)
     for emoji in emojis:
         yield from client.add_reaction(message,emoji)
-        yield from client.clear_reactions(message)
-    if any(shit_words) in message:
-         client.send_message(message.channel, 'you have entered the shit list.', tts=True)
-
-
+        yield from client.remove_reaction(message,emoji,client.user)
 
 client.run('MjkzMjMyMTMzMjgyMjAxNjAy.C7DqOw.ujB3abjJtzTkHHXf6hLXFGJ1UU0')
