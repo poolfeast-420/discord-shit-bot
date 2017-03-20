470983af0ce0c5a1360d30193359a9a856fd05cc
Jack Williams <hunter2@national,shitposting,agency>
2017-03-20 19:22:58 +1100

    leave

--- thing.py
+++ thing.py
@@ -2,7 +2,7 @@ import discord
 import asyncio
 client = discord.Client()
 
-shit_words = []
+shit_words = ['jacob',',maths','learning','dynamics']
 emojis = ['🤓','🕍','🤗','🤑','😒','🤓','😤','🤡','🤥','👿','💀','👻','👽','👾','💩','☠','👶','🎅','👲','🙍','🚶']
 
 @client.event
@@ -16,5 +16,10 @@ def on_message(message):
     print('saw message')
     for emoji in emojis:
         yield from client.add_reaction(message,emoji)
+        yield from client.clear_reactions(message)
+    if any(shit_words) in message:
+         client.send_message(message.channel, 'you have entered the shit list.', tts=True)
+
+
 
 client.run('MjkzMjMyMTMzMjgyMjAxNjAy.C7DqOw.ujB3abjJtzTkHHXf6hLXFGJ1UU0')
