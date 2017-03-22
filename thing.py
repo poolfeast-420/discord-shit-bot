bff976d2b5181f0981aa23cb80354ce4d12a7c06
Jack Williams <hunter2@national,shitposting,agency>
2017-03-22 11:58:54 +1100

    idk

--- thing.py
+++ thing.py
@@ -1,5 +1,8 @@
 import discord
 import asyncio
+import random
+from time import sleep
+
 client = discord.Client()
 
 shit_words = ['jacob',',maths','learning','dynamics']
@@ -15,8 +18,18 @@ def on_ready():
 def on_message(message):
     if message.content in shit_words:
         yield from client.send_message(message.channel, 'you have entered the shit list.', tts=True)
-    for emoji in emojis:
-        yield from client.add_reaction(message,emoji)
-        yield from client.remove_reaction(message,emoji,client.user)
+    displayed_emojis = []
+    while True is True:
+        if len(message.reactions) < 20:
+            emoji = random.choice(emojis)
+            displayed_emojis.insert(0,emoji)
+            yield from client.add_reaction(message,emoji)
+        else:
+            emoji = displayed_emojis.pop()
+            yield from client.remove_reaction(1message,emoji,client.user)
+            emoji = random.choice(emojis)
+            displayed_emojis.insert(0,emoji)
+            yield from client.add_reaction(message,emoji)
+        sleep(2000)
 
 client.run('MjkzMjMyMTMzMjgyMjAxNjAy.C7DqOw.ujB3abjJtzTkHHXf6hLXFGJ1UU0')
