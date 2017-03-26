322a9b0549c4ad4435d31de9c2c627f7914cb2b1
Jack Williams <hunter2@national,shitposting,agency>
2017-03-26 15:19:04 +1100

    no

--- thing.py
+++ thing.py
@@ -9,6 +9,7 @@ nice_words = ['pal','buddy','friendo','mate','chum','associate']
 shit_words = ['jacob','math','learning','dynamics','convolution','study','meme']
 emojis = ['🤓','🕍','🛂','⛳','🤗','🈶','🤑','😒','🤓','😤','🤡','🤥','👿','💀','👻','👽','👾','💩','☠','👶','🎅','👲','🙍','🚶','🙃']
 shit_list = []
+theres = ['their','theyre','they are','there','theire','thier',"they're"]
 
 @client.event
 @asyncio.coroutine
@@ -19,12 +20,18 @@ def on_ready():
 @asyncio.coroutine
 def on_message(message):
     print('Saw message')
-    for shit_word in shit_words:
-        if shit_word in message.content:
-            print('Message contained nasty things')
-            shit_list.append(message.author)
-            yield from client.send_message(message.channel, 'you have entered the shit list ' + random.choice(nice_words), tts=True)
-            yield from client.send_message(message.channel, 'the shit list contains ' + str(shit_list))
-    yield from client.add_reaction(message, random.choice(emojis))
+    if message.author != message.server.me:
+        for there in theres:
+            if there in message.content:
+                print('ohshitwaddup')
+                yield from client.send_message(message.channel, '*' + random.choice(theres), tts=True)
+                return
+        for shit_word in shit_words:
+            if shit_word in message.content:
+                print('Message contained nasty things')
+                shit_list.append(message.author)
+                yield from client.send_message(message.channel, 'you have entered the shit list ' + random.choice(nice_words), tts=True)
+                yield from client.send_message(message.channel, 'the shit list contains ' + str(shit_list))
+        yield from client.add_reaction(message, random.choice(emojis))
 
 client.run('MjkzMjMyMTMzMjgyMjAxNjAy.C7DqOw.ujB3abjJtzTkHHXf6hLXFGJ1UU0')
