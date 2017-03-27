9788e78f480184418c2a5fae3e91919513d31036
Jack Williams <hunter2@national.shitposting.agency>
2017-03-27 19:35:47 +1100

    no

--- thing.py
+++ thing.py
@@ -6,7 +6,7 @@ from time import sleep
 client = discord.Client()
 
 nice_words = ['pal','buddy','friendo','mate','chum','associate']
-shit_words = ['jacob','math','learning','dynamics','convolution','study','meme','bot','shit','#','richard','throat']
+shit_words = ['jacob','math','learning','dynamics','convolution','study','meme','bot','shit','#','richard','throat','laplace']
 emojis = ['🤓','🕍','🛂','⛳','🤗','🈶','🤑','😒','🤓','😤','🤡','🤥','👿','💀','👻','👽','👾','💩','☠','👶','🎅','👲','🙍','🚶','🙃']
 shit_list = []
 theres = ['their','theyre','they are','there','theire','thier',"they're"]
