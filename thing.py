73aea906a71d477fb3f1e53daace65dfcf486f33
Conman <richardharris443@gmail.com>
2017-03-27 20:25:45 +1100

    Update thing.py

--- thing.py
+++ thing.py
@@ -2,6 +2,9 @@ import discord
 import asyncio
 import random
 from time import sleep
+import time
+startTime = time.time()
+endTime = startTime + 9:15
 
 client = discord.Client()
 
@@ -33,5 +36,7 @@ def on_message(message):
                 yield from client.send_message(message.channel, 'you have entered the shit list ' + random.choice(nice_words), tts=True)
                 yield from client.send_message(message.channel, 'the shit list contains ' + str(shit_list))
         yield from client.add_reaction(message, random.choice(emojis))
+        
+if rand(startTime,endTime) > (previous_comment - time.time())
 
 client.run('MjkzMjMyMTMzMjgyMjAxNjAy.C7DqOw.ujB3abjJtzTkHHXf6hLXFGJ1UU0')
