fc0da768a651819db9af0f127ba75ce8f66776e7
Conman <richardharris443@gmail.com>
2017-03-29 20:04:44 +1100

    Update thing.py
    adding the redefining of last_message_time

--- thing.py
+++ thing.py
@@ -29,7 +29,7 @@ def on_message(message):
                 yield from client.send_message(message.channel, str.join("", formatted_list))       
         if (time.time() - last_message_time) > randit(10800,65628): 
             yield from client.send_message(message.channel, random.choice(daily_grilling), tts=True)
-
+    last_message_time = current_time;
         yield from client.add_reaction(message, random.choice(emojis))
         
     #checks if the user has asked the bot a question, then give a random response from response list    
