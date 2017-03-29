3a7e75e898723b4d4ef07f5ea72b02c82cf935a3
Conman <richardharris443@gmail.com>
2017-03-29 19:18:54 +1100

    Update thing.py
    setting up the daily grilling

--- thing.py
+++ thing.py
@@ -5,10 +5,6 @@ import time
 
 client = discord.Client() 
 
-startTime = time.time();
-endTime = startTime + 34980;
-commentTime = time.time();
-
 @client.event
 @asyncio.coroutine
 def on_ready():
@@ -23,9 +19,6 @@ def on_message(message):
             if there in message.content:
                 print('ohshitwaddup')
                 yield from client.send_message(message.channel, '*' + random.choice(theres), tts=True)
-                
-    if message.author != message.content:            
-    
         for shit_word in shit_words:
             if shit_word in message.content.lower():
                 print('Message contained nasty things')
@@ -33,13 +26,17 @@ def on_message(message):
                 formatted_list = ["{0}\n".format(member) for member in shit_list]
                 yield from client.send_message(message.channel, 'you have entered the shit list ' + random.choice(nice_words), tts=True)
                 yield from client.send_message(message.channel, str.join("", formatted_list))
+        #Richards time function        
+        for message.author:
+            lowerlimitTime = time.time();
+            upperlimitTime = commentTime + 74980;
+                if rand(lowerlimitTime,upperlimitTime) > (previous_comment - time.time()):
+                    yield from client.send_message(message.channel, random.choice(daily_grilling), tts=True)
+
         yield from client.add_reaction(message, random.choice(emojis))
         
     #checks if the user has asked the bot a question, then give a random response from response list    
-    if question_words and name_words in message.content.lower():
+    if question_words message.content.lower():
         yield from client.send_message(message.channel, random.choice(question_responses), tts=True)
-    #picks a random time from within provided set, later get it to respond to next messager in chat                     
-    if rand(startTime,EndTime) > (previous_comment - time.time()):
-        yield from client.send_message(message.channel, random.choice(daily_grilling), tts=True)
         
 client.run('MjkzMjMyMTMzMjgyMjAxNjAy.C7DqOw.ujB3abjJtzTkHHXf6hLXFGJ1UU0')
