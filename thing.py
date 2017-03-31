fb0005f11aca26c9b56a58126763f47d03363077
Jack Williams <hunter2@national,shitposting,agency>
2017-03-31 20:10:08 +1100

    added nice things and stuff

--- thing.py
+++ thing.py
@@ -1,32 +1,27 @@
 import discord
 import asyncio
 import random
-from wordlist import
-
+from words import search, vocabulary
 
 client = discord.Client()
-
-
-@client.event
-@asyncio.coroutine
-def on_ready():
-    print('Ready')
+shit_list = []
 
 @client.event
 @asyncio.coroutine
 def on_message(message):
     if message.author != message.server.me:
-        for there in theres:
-            if there in message.content.lower():
-                yield from client.send_message(message.channel, '*' + random.choice(theres), tts=True)
+        for wordlist in search:
+            for word in wordlist:
+                if word in message.content.lower():
+                    if wordlist is 'theres':
+                        yield from client.send_message(message.channel, '*' + random.choice(search['theres']), tts=True)
+                    if wordlist is 'shit':
+                        shit_list.append(message.author.nick)
+                        formatted_list = ["{0} x{1}\n".format(member,shit_list.count(member)) for member in set(shit_list)]
+                        yield from client.send_message(message.channel, 'you have entered the shit list ' + random.choice(vocabulary['nice']) + ' ' + random.choice(vocabulary['friend']), tts=True)
+                        yield from client.send_message(message.channel, str.join("", formatted_list), tts=True)
         if message.server.me.nick.lower() in message.content.lower():
                 yield from client.send_message(message.channel,'fuk u')
-        for shit_word in shit_words:
-            if shit_word in message.content.lower():
-                shit_list.append(message.author.nick)
-                formatted_list = ["{0} x{1}\n".format(member,shit_list.count(member)) for member in set(shit_list)]
-                yield from client.send_message(message.channel, 'you have entered the shit list ' + random.choice(nice_words) + ' ' + random.choice(friend_words), tts=True)
-                yield from client.send_message(message.channel, str.join("", formatted_list), tts=True)
-        yield from client.add_reaction(message, random.choice(emojis))
+        yield from client.add_reaction(message, random.choice(vocabulary['emojis']))
 
 client.run('MjkzMjMyMTMzMjgyMjAxNjAy.C7DqOw.ujB3abjJtzTkHHXf6hLXFGJ1UU0')
