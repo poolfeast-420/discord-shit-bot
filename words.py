3554127def4bbbaf9a79150f307d17f4445a5bb4
Jack Williams <hunter2@national.shitposting.agency>
2017-03-31 20:45:44 +1100

    Update words.py

--- words.py
+++ words.py
@@ -1,10 +1,15 @@
 vocabulary = {
-'friend':['pal','buddy','boi','friendo','mate','chum','associate','amigo','hombre','compadre','comrade','homeboy','partner','main man','bosom pal','informal homeboy','informal gabba','absolute no life little bitch human trash mega faggot'],
-'emojis':['🤓','🕍','🛂','⛳','🤗','🈶','🤑','😒','🤓','😤','🤡','🤥','👿','💀','👻','👽','👾','💩','☠','👶','🎅','👲','🙍','🚶','🙃'],
-'nice':['cool','radical','sweet','beautiful','gorgeous','perfect','amazing','irresistible','tasty','delicious','signor','divine','knockout','weeb trash','ravishing','heavenly','foxy','smashing','autistic','vile','degenerate','crispy','ripe','hygenic']
+    'friend':
+        ['pal','buddy','boi','friendo','mate','chum','associate','amigo','hombre','compadre','comrade','homeboy','partner','main man','bosom pal','informal homeboy','informal gabba','absolute no life little bitch human trash mega faggot'],
+    'emojis':
+        ['🤓','🕍','🛂','⛳','🤗','🈶','🤑','😒','🤓','😤','🤡','🤥','👿','💀','👻','👽','👾','💩','☠','👶','🎅','👲','🙍','🚶','🙃'],
+    'nice':
+        ['cool','radical','sweet','beautiful','gorgeous','perfect','amazing','irresistible','tasty','delicious','signor','divine','knockout','weeb trash','ravishing','heavenly','foxy','smashing','autistic','vile','degenerate','crispy','ripe','hygenic']
 }
 
 search = {
-'shit':['jacob','math','learning','dynamics','matlab','convolution','study','meme','bot','shit','#','richard','throat','laplace','boi','lmao','lol','wow','rofl','doge'],
-'theres':['their','theyre','they are','there','theire','thier',"they're",'theirie','bear','thire']
+    'shit':
+        ['jacob','math','learning','dynamics','matlab','convolution','study','meme','bot','shit','#','richard','throat','laplace','boi','lmao','lol','wow','rofl','doge'],
+    'theres':
+        ['their','theyre','they are','there','theire','thier',"they're",'theirie','bear','thire']
 }
