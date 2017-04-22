ae3035cb5cd26cc45e9927cfb1af6ffd3e3a37f1
Conman <richardharris443@gmail.com>
2017-04-22 12:38:30 +1000

    Update thing.py
    wdwasgyuiawigbD

--- thing.py
+++ thing.py
@@ -7,10 +7,10 @@ from words import search, vocabulary
 from eventmaker import eventdetector, profilepicture, specialprofilechecker
 from urllib.request import Request, urlopen
 
-
 client = discord.Client()
 shit_list = []
 most_recent_channel = None
+timerthingy = time.time()
 
 @client.event
 @asyncio.coroutine
@@ -27,6 +27,12 @@ def on_message(received_message):
                 comment = vocabulary['bee']
                 for split_message in [comment[character:character+1500] for character in range(0, len(comment), 1500)]:
                     yield from client.send_message(received_message.channel, split_message)
+                        if specialprofilechecker is True:
+            if (time.time() - timerthingy) > 300:
+                timerthingy = time.time()
+                yield from client.send_message(message.channel, random.choice(eventdetector), tts=True)
+            if specialprofilechecker is True:
+                yield from client.edit_profile(avatar=urlopen(profilepicture).read())         
             if received_message.content.lower().startswith('hi'):
                 avatar = urlopen(Request(received_message.author.avatar_url.replace('webp','jpeg'), headers={'User-Agent': 'Mozilla/5.0'})).read()
                 if avatar is not received_message.server.me.avatar:
@@ -64,6 +70,9 @@ def on_message(received_message):
                         if wordlist_name in ['hitler','ussr']:
                             for phrase in vocabulary[wordlist_name]:
                                 yield from client.send_message(received_message.channel, phrase)
+                        if wordlist_name is 'instadeletecomments':
+                            yousucktimer = time.time()
+                            shitauthor = message.author        
                         if wordlist_name is 'learning':
                             yield from client.send_message(received_message.channel,'existence is pain')
                         if wordlist_name is 'friend':
@@ -72,5 +81,9 @@ def on_message(received_message):
                 yield from client.add_reaction(received_message, random.choice(vocabulary['emojis']))
             else:
                 yield from client.add_reaction(received_message, '💩')
+                        while (time.time() - yousucktimer) > 300: 
+            if message.author == shitauthor:
+                yield from client.delete_message(message)
+                yield from client.send_message(message.channel, 'Did you guys hear something?', tts=True)     
 
 client.run()
