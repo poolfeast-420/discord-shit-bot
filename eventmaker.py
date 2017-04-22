d54f7fa7edf19dffc9a77c24d567e38d0362a343
Conman <richardharris443@gmail.com>
2017-04-22 18:10:28 +1000

    Update eventmaker.py
    ()())())(

--- eventmaker.py
+++ eventmaker.py
@@ -6,64 +6,64 @@ currentyear = datetime.datetime.now().year
 currentmonth = datetime.datetime.now().month
 currentday = datetime.datetime.now().day
 
-if (datetime.datetime().now == datetime.date(currentyear,25,12)):
+if (datetime.datetime.now() == datetime.date(currentyear,25,12)):
     evetdetector =  eventsaying['christmas']
     profilepicture = eventphoto['christmas']
-elif (datetime.datetime().now == datetime.date(currentyear,26,1)):
+elif (datetime.datetime.now() == datetime.date(currentyear,26,1)):
     eventdetector = eventsaying['stryaday']
     profilepicture = eventphoto['stryaday']
-elif (datetime.datetime().now == datetime.date(currentyear,20,3)):
+elif (datetime.datetime.now() == datetime.date(currentyear,20,3)):
     eventdetector = eventsaying['alienabduction']
     profilepicture = eventphoto['alienabduction']
-elif (datetime.datetime().now == datetime.date(currentyear,3,5)):
+elif (datetime.datetime.now() == datetime.date(currentyear,3,5)):
     eventdetector = eventsaying['lumpyrug']
     profilepicture = eventphoto['lumpyrug']
-elif (datetime.datetime().now == datetime.date(currentyear,22,2)):
+elif (datetime.datetime.now() == datetime.date(currentyear,22,2)):
     eventdetector = eventsaying['humble']
     profilepicture = eventphoto['humble']
-elif (datetime.datetime().now == datetime.date(currentyear,15,4)):
+elif (datetime.datetime.now() == datetime.date(currentyear,15,4)):
     eventdetector = eventsaying['thatsucks']
     profilepicture = eventphoto['thatsucks']
-elif (datetime.datetime().now == datetime.date(currentyear,19,11)):
+elif (datetime.datetime.now() == datetime.date(currentyear,19,11)):
     eventdetector = eventsaying['haveabadday']
     profilepicture = eventphoto['haveabadday']
-elif (datetime.datetime().now == datetime.date(currentyear,11,9)):
+elif (datetime.datetime.now() == datetime.date(currentyear,11,9)):
     eventdetector = eventsaying['911']
     profilepicture = eventphoto['911']
-elif (datetime.datetime().now == datetime.date(currentyear,8,1)):
+elif (datetime.datetime.now() == datetime.date(currentyear,8,1)):
     eventdetector = eventsaying['kimjongun']
     profilepicture = eventphoto['kimjongun']
-elif (datetime.datetime().now == datetime.date(currentyear,4,5)):
+elif (datetime.datetime.now() == datetime.date(currentyear,4,5)):
     eventdetector = eventsaying['force']
     profilepicture = eventphoto['force']
-elif (datetime.datetime().now == datetime.date(currentyear,4,3)):
+elif (datetime.datetime.now() == datetime.date(currentyear,4,3)):
     eventdetector = eventsaying['holyexperiment'] 
     profilepicture = eventphoto['holyexperiment']
-elif (datetime.datetime().now == datetime.date(currentyear,13,2)):
+elif (datetime.datetime.now() == datetime.date(currentyear,13,2)):
     eventdetector = eventsaying['pancakeday']
     profilepicture = eventphoto['pancakeday']
-elif (datetime.datetime().now == datetime.date(currentyear,18,12)):
+elif (datetime.datetime.now() == datetime.date(currentyear,18,12)):
     eventdetector = eventsaying['stalin']
     profilepicture = eventphoto['stalin']
-elif (datetime.datetime().now == datetime.date(currentyear,20,4)):
+elif (datetime.datetime.now() == datetime.date(currentyear,20,4)):
     eventdetector = eventsaying['hitler'] 
     profilepicture = eventphoto['hitler']
-elif (datetime.datetime().now == datetime.date(currentyear,15,1)):
+elif (datetime.datetime.now() == datetime.date(currentyear,15,1)):
     eventdetector = eventsaying['martinluther'] 
     profilepicture = eventphoto['martinluther']
-elif (datetime.datetime().now == datetime.date(currentyear,13,4)):
+elif (datetime.datetime.now() == datetime.date(currentyear,13,4)):
     eventdetector = eventsaying['hitchens']
     profilepicture = eventphoto['hitchens']
-elif (datetime.datetime().now == datetime.date(currentyear,26,7)):
+elif (datetime.datetime.now() == datetime.date(currentyear,26,7)):
     eventdetector = eventsaying['kubrick']
     profilepicture = eventphoto['kubrick']
-elif (datetime.datetime().now == datetime.date(currentyear,14,3)):
+elif (datetime.datetime.now() == datetime.date(currentyear,14,3)):
     eventdetector = eventsaying['einstein'] 
     profilepicture = eventphoto['einstein']
-elif (datetime.datetime().now == datetime.date(currentyear,12,2)):
+elif (datetime.datetime.now() == datetime.date(currentyear,12,2)):
     eventdetector = eventsaying['darwin'] 
     profilepicture = eventphoto['darwin']
-elif (datetime.datetime().now == datetime.date(currentyear,12,2)):
+elif (datetime.datetime.now() == datetime.date(currentyear,12,2)):
     eventdetector = eventsaying['Schrödinger'] 
     profilepicture = eventphoto['Schrödinger']
 else: 
