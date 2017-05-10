e8a38355a638f3851cf3811d3c23bb9b0138c8ca
Conman <richardharris443@gmail.com>
2017-05-10 20:36:01 +1000

    Revert "Revert "DOIng it JAcks way, i hope your happy""
    This reverts commit a58f6b7804cce03ab1955f76904cd9e7f7ae25f6.

--- events.py
+++ events.py
@@ -14,7 +14,10 @@ events = [
         'date':
             date(1,12,25),
         'comment':
-            'fuk u all'
+            'fuk u all',
+        'avatar':
+            'https://s-media-cache-ak0.pinimg.com/originals/ac/6c/63/ac6c63f0db0852f0353a3d58a627ad3b.png'
+            
     },{
         'name':
             "the day jack tested this on",
@@ -24,5 +27,204 @@ events = [
             'rip me',
         'avatar':
             'http://scontent.cdninstagram.com/t51.2885-15/s480x480/e35/16465838_1908759332685188_3341097279086919680_n.jpg?ig_cache_key=MTQ0NTA2MjM0NTY4MDE2NDQ3OQ%3D%3D.2'
+    },{
+        'name':
+            "stryaday",
+        'date':
+            date(1770,1,26),
+        'comment':
+            'strya day hell yeah',
+        'avatar':
+            'https://pbs.twimg.com/media/B8Vtlh6CIAALHLR.png'    
+    },{
+        'name':
+            "alienabduction",
+        'date':
+            date(1961,3,20),
+        'comment':
+            'man theres aliens out there',
+        'avatar':
+            'https://d30y9cdsu7xlg0.cloudfront.net/png/12346-200.png'
+    },{
+        'name':
+            "lumpyrug",
+        'date':
+            date(1997,5,3),
+        'comment':
+            ['oh boi this rug here is real lumpy','I said, Lump off','you can\'t handle these lumps'],
+        'avatar':
+            'http://vignette3.wikia.nocookie.net/adventure-time/images/8/8c/Lumpy_Space_Princess.png/revision/latest?cb=20110510142353'
+    },{
+        'name':
+            "humble",
+        'date':
+            date(1997,2,22),
+        'comment':
+            'and you are humble as well',
+        'avatar':
+            'https://fintech4us.files.wordpress.com/2016/08/im-humble-722111.png?w=662'
+    },{
+        'name':
+            "thatsucks",
+        'date':
+            date(1770,4,15),
+        'comment':
+            ['why is everything so shitty','everything you do sucks'],
+        'avatar':
+            'http://icons.iconarchive.com/icons/iconsmind/outline/512/Depression-icon.png'    
+    },{
+        'name':
+            "haveabadday",
+        'date':
+            date(1961,11,19),
+        'comment':
+            'i hope your day is as shitty as you are',
+        'avatar':
+            'https://image.flaticon.com/icons/png/512/42/42305.png'
+    },{
+        'name':
+            "911",
+        'date':
+            date(2001,9,11),
+        'comment':
+            ['hmmm, its my favourite time of year agian','jet fuel doesn\'t melt steel beams','911 was an inside job'],
+        'avatar':
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/8/8a/UA_Flight_175_hits_WTC_south_tower_9-11_edit.jpeg/1024px-UA_Flight_175_hits_WTC_south_tower_9-11_edit.jpeg'
+    },{
+        'name':
+            "kimjongun",
+        'date':
+            date(1984,1,8),
+        'comment':
+            ['happy birthday to our favourite chubby dictactor','kim is love, kim is life'],
+        'avatar':
+            'http://www.agarioskins.org/wp-content/uploads/2015/07/kim-jong-un1.png'
+    },{
+        'name':
+            "force",
+        'date':
+            date(1977,5,4),
+        'comment':
+            ['do it','may the forth be with you'],
+        'avatar':
+            'http://vignette2.wikia.nocookie.net/swfanon/images/5/53/Rebel_Emblem.png/revision/latest?cb=20100820114718'
+    },{
+        'name':
+            "holyexperiment",
+        'date':
+            date(1,3,4),
+        'comment':
+            ['Pray someone will ask you on a date','Try to get a person to attend religious services','Plan to convert someone to your religion','Kill a Non-Believer'],
+        'avatar':
+            'http://www.thetablet.co.uk/UserFiles/images/news/300x300/pell_cardinal_vatican_300.png'    
+    },{
+        'name':
+            "pancakeday",
+        'date':
+            date(1439,2,13),
+        'comment':
+            ['you better be eating some pancakes','oh i get it, flying saucer shaped pancakes are regular pancakes'],
+        'avatar':
+            'http://37.media.tumblr.com/605762b2e80fbdda35364d5b0db4a540/tumblr_n5bxf2lI5k1rx3d1wo1_r2_500.png'
+    },{
+        'name':
+            "stalin",
+        'date':
+            date(1878,12,18),
+        'comment':
+            ['Его усы такие же могучие, как и его решения\n░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░\n░░░░░░░░░░▀▀▀██████▄▄▄░░░░░░░░░░\n░░░░░░░░░░░░░░░░░▀▀▀████▄░░░░░░░\n░░░░░░░░░░▄███████▀░░░▀███▄░░░░░\n░░░░░░░░▄███████▀░░░░░░░▀███▄░░░\n░░░░░░▄████████░░░░░░░░░░░███▄░░\n░░░░░██████████▄░░░░░░░░░░░███▌░\n░░░░░▀█████▀░▀███▄░░░░░░░░░▐███░\n░░░░░░░▀█▀░░░░░▀███▄░░░░░░░▐███░\n░░░░░░░░░░░░░░░░░▀███▄░░░░░███▌░\n░░░░▄██▄░░░░░░░░░░░▀███▄░░▐███░░\n░░▄██████▄░░░░░░░░░░░▀███▄███░░░\n░█████▀▀████▄▄░░░░░░░░▄█████░░░░\n░████▀░░░▀▀█████▄▄▄▄█████████▄░░\n░░▀▀░░░░░░░░░▀▀██████▀▀░░░▀▀██░░\n░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░'],
+        'avatar':
+            'http://orig07.deviantart.net/0bb4/f/2010/205/6/5/it__sa_me__a_stalin_by_irae8.png'
+    },{
+        'name':
+            "hitler",
+        'date':
+            date(1889,4,20),
+        'comment':
+            ['Herzlichen Glückwunsch zum Geburtstag und Herrschaft\n😂😂😂💯💯💯💯💯😂😂😂😂😂😂😂😂😂😂😂\n😂😂😂💯💯💯💯💯😂😂😂😂😂😂😂😂😂😂😂\n😂😂😂💯💯💯💯💯😂😂😂😂😂😂😂😂😂😂😂\n😂😂😂💯💯💯💯💯😂😂😂💯💯💯💯💯💯💯💯\n😂😂😂💯💯💯💯💯😂😂😂💯💯💯💯💯💯💯💯\n😂😂😂💯💯💯💯💯😂😂😂💯💯💯💯💯💯💯💯\n😂😂😂💯💯💯💯💯😂😂😂💯💯💯💯💯💯💯💯\n😂😂😂💯💯💯💯💯😂😂😂💯💯💯💯💯💯💯💯\n😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂\n😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂\n','😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂\n💯💯💯💯💯💯💯💯😂😂😂💯💯💯💯💯😂😂😂\n💯💯💯💯💯💯💯💯😂😂😂💯💯💯💯💯😂😂😂\n💯💯💯💯💯💯💯💯😂😂😂💯💯💯💯💯😂😂😂\n💯💯💯💯💯💯💯💯😂😂😂💯💯💯💯💯😂😂😂\n💯💯💯💯💯💯💯💯😂😂😂💯💯💯💯💯😂😂😂\n😂😂😂😂😂😂😂😂😂😂😂💯💯💯💯💯😂😂😂\n😂😂😂😂😂😂😂😂😂😂😂💯💯💯💯💯😂😂😂\n😂😂😂😂😂😂😂😂😂😂😂💯💯💯💯💯😂😂😂'],
+        'avatar':
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/Standarte_Adolf_Hitlers.svg/670px-Standarte_Adolf_Hitlers.svg.png'
+    },{
+        'name':
+            "martinluther",
+        'date':
+            date(1929,1,15),
+        'comment':
+            ['I have a dream that...'],
+        'avatar':
+            'https://s-media-cache-ak0.pinimg.com/originals/b2/38/98/b2389834ba98ee1f94e17df112377f6e.png'    
+    },{
+        'name':
+            "hitchens",
+        'date':
+            date(1949,4,13),
+        'comment':
+            ['Who the hell are you?','see what brain-rot decends on people','Alcohol makes other people less tedious'],
+        'avatar':
+            'http://4.bp.blogspot.com/-3pPECBgI02M/T5vil4t3M2I/AAAAAAAACBU/_oxSgbDoH8Q/s1600/christopher_hitchens_by_ee7-d4pcu3e.png'
+    },{
+        'name':
+            "kubrick",
+        'date':
+            date(1928,7,26),
+        'comment':
+            ['heres johnny','i didn\'t fake the moon landing'],
+        'avatar':
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f6/HAL9000.svg/256px-HAL9000.svg.png'
+    },{
+        'name':
+            "einstein",
+        'date':
+            date(1879,3,14),
+        'comment':
+            ['e = m x c^2 motherfucker','i vill mess with time, i vill'],
+        'avatar':
+            'http://vignette4.wikia.nocookie.net/documentales/images/d/d6/Einstein.png/revision/latest?cb=20130722012610&path-prefix=es'
+    },{
+        'name':
+            "darwin",
+        'date':
+            date(1809,2,12),
+        'comment':
+            ['evolution is just a \'theory\' a game theory','did you just call me a monkey'],
+        'avatar':
+            'https://siriusknotts.files.wordpress.com/2008/08/darwin_the_monkey1.png'
+    },{
+        'name':
+            "Schrödinger",
+        'date':
+            date(1887,8,12),
+        'comment':
+            ['oh boi here i go killing cats again','if i put you in a box where you can\'t be observed, are you still a retard or not?'],
+        'avatar':
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c8/Schroedingers_cat_film.svg/544px-Schroedingers_cat_film.svg.png'    
+    },{
+        'name':
+            "rainbow",
+        'date':
+            date(2015,4,26),
+        'comment':
+            ['One big fucking hole coming right up','FUCK OFF PULSE!!!','I\'m an Engineer not a Doctor','Lets heal that booboo'],
+        'avatar':
+            'http://orig10.deviantart.net/5918/f/2015/177/2/7/rainbow_six_siege_dock_icon_by_outlawninja-d8yvme8.png'
+    },{
+        'name':
+            "voltaire",
+        'date':
+            date(1887,8,12),
+        'comment':
+            ['Optimism is the madness of insisting that all is well when we are miserable.',"\"Now, now my good man, this is no time to be making enemies.\"
+(Voltaire on his deathbed in response to a priest asking him that he renounce Satan.)"],
+        'avatar':
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c8/Schroedingers_cat_film.svg/544px-Schroedingers_cat_film.svg.png'    
+    },{
+        'name':
+            "dawkins",
+        'date':
+            date(1941,3,26),
+        'comment':
+            ['Science, it works, bitches','By all means let\'s be open-minded, but not so open-minded that our brains drop out','Science is interesting, if you dont agree you can fuck off'],
+        'avatar':
+            'http://i.ytimg.com/vi/YfAZY7LaPYA/hqdefault.jpg'
     }
 ]
