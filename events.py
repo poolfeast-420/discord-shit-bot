a1002f5629b825aa1c53a756300f278b5140dba5
Jack Williams <jack@Mac-mini.local>
2017-05-10 19:01:01 +1000

    lmao

---
+++ events.py
@@ -0,0 +1,28 @@
+from datetime import datetime as date
+
+events = [
+    {
+        'name':
+            "jack's birthday",
+        'date':
+            date(1997,8,15),
+        'comment':
+            'elmayo'
+    },{
+        'name':
+            "christmas",
+        'date':
+            date(1,12,25),
+        'comment':
+            'fuk u all'
+    },{
+        'name':
+            "the day jack tested this on",
+        'date':
+            date(2017,5,10),
+        'comment':
+            'rip me',
+        'avatar':
+            'http://scontent.cdninstagram.com/t51.2885-15/s480x480/e35/16465838_1908759332685188_3341097279086919680_n.jpg?ig_cache_key=MTQ0NTA2MjM0NTY4MDE2NDQ3OQ%3D%3D.2'
+    }
+]
--- thing.py
+++ thing.py
@@ -1,7 +1,9 @@
 import discord
 import asyncio
 import random
-import datetime
+from datetime import datetime
+current_date = datetime.now
+from events import events
 from words import search, vocabulary
 from urllib.request import Request, urlopen
 
@@ -10,18 +12,30 @@ shit_list = []
 most_recent_channel = None
 
 @asyncio.coroutine
-def background_task():
+def timer_update():
     yield from client.wait_until_ready()
+    # This section is run at startup
     yield from client.change_presence(status=discord.Status.invisible)
+    completed_events = []
     while not client.is_closed:
-        yield from client.send_message(discord.Object(id='228814605923647488'),random.choice(vocabulary['nice']))
-        yield from asyncio.sleep(60*60)
+        # This section runs continiously every minute (but also at startup)
+        for event in events:
+            if event['date'].day is current_date().day and event['date'].month is current_date().month and not(event in completed_events):
+                completed_events = completed_events + [event]
+                if 'comment' in event:
+                    yield from client.send_message(discord.Object(id='228814605923647488'),event['comment'])
+                if 'avatar' in event:
+                    avatar = urlopen(Request(event['avatar'], headers={'User-Agent': 'Mozilla/5.0'})).read()
+                    yield from client.edit_profile(avatar=avatar)
+        yield from asyncio.sleep(60)
 
 @client.async_event
 def on_message(received_message):
     if received_message.channel.is_private:
+        # This section runs whenever a private message is received
          yield from client.send_message(last_message.channel, received_message.content)
     else:
+        # This section runs whenever a public message is received
         last_message = received_message
         emojis = random.choice(vocabulary['emojis'])
         if received_message.author != received_message.server.me:
@@ -41,8 +55,8 @@ def on_message(received_message):
                 yield from client.delete_message(received_message)
                 yield from client.send_message(received_message.channel, received_message.content + ' also i enjoy penis')
                 return # Stop message processing here, because the message is gone, and we don't won't to compromise our identity
-            if str(int(datetime.datetime.now().minute)) + 'clear' in received_message.content.lower():
-                yield from client.purge_from(received_message.channel, before=datetime.datetime.now() - datetime.timedelta(minutes=15), check=lambda message:received_message.author == client.user)
+            if str(int(current_date().minute)) + 'clear' in received_message.content.lower():
+                yield from client.purge_from(received_message.channel, before=current_date() - datetime.timedelta(minutes=15), check=lambda message:received_message.author == client.user)
                 yield from client.delete_message(received_message)
                 return
             for wordlist_name, words in search.items():
@@ -76,5 +90,5 @@ def on_message(received_message):
             for emoji in emojis:
                 yield from client.add_reaction(received_message, emoji)
 
-client.loop.create_task(background_task())
+client.loop.create_task(timer_update())
 client.run(input('token: '))
