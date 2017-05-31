6313d3ab1296730d38eb65756aa328981c9b7170
Jack Williams <hunter2@national.shitposting.agency>
2017-05-31 15:34:26 +1000

    Update eiuaeijdeane.py

--- eiuaeijdeane.py
+++ eiuaeijdeane.py
@@ -4,7 +4,7 @@ from words import vocabulary
 
 class AI_Thread(Process):
     def __init__(self, send_queue, receive_queue):
-        print('Starting AI')
+        print('oawijfaof;a3r3qafisepfqw3fp92p9f')
         Process.__init__(self)
         self.shitbot = ChatBot(
             'shit bot',
@@ -16,15 +16,15 @@ class AI_Thread(Process):
         self.exit = Event()
         self.receive_queue = receive_queue
         self.send_queue = send_queue
-        print('AI Loaded')
+        print('aojndio;andio;ani;dfeaifbitbfi;e')
 
     def run(self):
-        print('AI Running')
+        print('ijwdoaw9diuao;iwdho;ajep9rd3')
         while not self.exit.is_set():
             message = self.receive_queue.get()
             new_message = self.shitbot.get_response(message)
             self.send_queue.put(new_message)
 
     def terminate(self):
-        print('AI Stopped')
+        print('eoaduhawiourp9adp94nqwufndp923hr9wnof;ne')
         self.exit.set()
